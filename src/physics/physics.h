d8caa8a05ac1729523b89cfd64c54fd906692dd5
mattlo3 <47372402+mattlo3@users.noreply.github.com>
2021-02-11 20:32:10 -0700

    player1 information added
    physics passes information from vehicle to main

--- src/physics/physics.h
+++ src/physics/physics.h
@@ -1,8 +1,11 @@
 #pragma once
 #include <GLFW/glfw3.h>
+#include <foundation/PxTransform.h>
+#include <foundation/Px.h>
 
-void initPhysics();
 
-void stepPhysics(GLFWwindow* window);
+void initPhysics(physx::PxTransform player);
+
+void stepPhysics(GLFWwindow* window, physx::PxTransform player);
 
 void cleanupPhysics();
