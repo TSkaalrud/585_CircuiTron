68ea40b64f5e4dc081a32e9867dd9ae38f656e49
Daniel Doran <ddoran3@gmail.com>
2021-02-11 20:56:17 -0700

    references, references everywhere!

--- src/entities/car.hpp
+++ src/entities/car.hpp
@@ -3,29 +3,28 @@
 #include "game_object.hpp"
 
 class Car : public GameObject {
-	private:
-		int lap = 1;
-		int checkpoint = 0;
-		int place = 0;
+  private:
+	int lap = 1;
+	int checkpoint = 0;
+	int place = 0;
 
-	public:
-		// change assets/test.glb to car model path
-		Car(Render::Render& render, int start_place, physx::PxTransform pt) : GameObject(render, "assets/Bike_Final.glb", pt) { 
-			place = start_place;
-		};
+  public:
+	// change assets/test.glb to car model path
+	Car(Render::Render& render, int start_place, physx::PxTransform& pt)
+		: GameObject(render, "assets/Bike_Final.glb", pt), place(start_place){};
 
-		// not sure if this would go here
-		void SpawnWall() {
-			// spawn wall behind car
-		}
-		
-		void setPlace(int n) { place = n; }
-		int getPlace() { return place; }
+	// not sure if this would go here
+	void SpawnWall() {
+		// spawn wall behind car
+	}
 
-		void addLap(int n) { lap++; }
-		int getLap() { return lap; }
+	void setPlace(int n) { place = n; }
+	int getPlace() { return place; }
 
-		void addCheckpoint() { checkpoint++; }
-		void resetCheckpoint() { checkpoint = 0; }
-		int getCheckpoint() { return checkpoint; }
+	void addLap(int n) { lap++; }
+	int getLap() { return lap; }
+
+	void addCheckpoint() { checkpoint++; }
+	void resetCheckpoint() { checkpoint = 0; }
+	int getCheckpoint() { return checkpoint; }
 };
--- src/entities/game_object.hpp
+++ src/entities/game_object.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
 #include "entity.hpp"
-#include "render/render.hpp"
 #include "render/model_import.hpp"
+#include "render/render.hpp"
 
 #include "util.hpp"
 
@@ -13,40 +13,37 @@
 #include <optional>
 
 class GameObject : public Entity {
-	private:
-		Render::Render& render;
-		std::string model_path;
-		physx::PxTransform transform;
-		std::optional<Render::GroupInstance> model;
+  private:
+	Render::Render& render;
+	std::string model_path;
+	physx::PxTransform& transform;
+	std::optional<Render::GroupInstance> model;
 
-	public:
-		GameObject(Render::Render& r, std::string mp, physx::PxTransform& pt) : render(r) {
-			model_path = mp;
-			transform = pt;
-		};
+  public:
+	GameObject(Render::Render& r, std::string mp, physx::PxTransform& pt) : render(r), model_path(mp), transform(pt){};
 
-		void enter() override {
-			render.camera_set_fov(50);
+	void enter() override {
+		render.camera_set_fov(50);
 
-			Render::Group group = importModel(model_path, render);
+		Render::Group group = importModel(model_path, render);
 
-			model.emplace(group);
+		model.emplace(group);
 
-			float pi = glm::pi<float>();
-			render.create_dir_light({pi, pi, pi}, {1, 1, 1});
-			render.create_dir_light({2, 2, 2}, {-1, 0, 1});
-			render.create_dir_light({1, 1, 1}, {0, 0, -1});
-		}
+		float pi = glm::pi<float>();
+		render.create_dir_light({pi, pi, pi}, {1, 1, 1});
+		render.create_dir_light({2, 2, 2}, {-1, 0, 1});
+		render.create_dir_light({1, 1, 1}, {0, 0, -1});
+	}
 
-		void update(float deltaTime) override {
-			const float dist = 10;
-			const float speed = 1;
+	void update(float deltaTime) override {
+		const float dist = 10;
+		const float speed = 1;
 
-			glm::vec3 camera = {10, 5, 0};
-			glm::mat4 cameraPos = lookAt(camera, glm::vec3{0, 0, 0}, glm::vec3{0, 1, 0});
+		glm::vec3 camera = {10, 5, 0};
+		glm::mat4 cameraPos = lookAt(camera, glm::vec3{0, 0, 0}, glm::vec3{0, 1, 0});
 
-			render.camera_set_pos(cameraPos);
+		render.camera_set_pos(cameraPos);
 
-			model -> setTransform(convertTransform(transform));
-		}
+		model->setTransform(convertTransform(transform));
+	}
 };
