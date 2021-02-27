9dde1a45e85b60eddb19c38db72fa9266b8c1958
Daniel Doran <ddoran3@gmail.com>
2021-02-26 17:55:56 -0700

    skybox texture parameters

--- src/render/model_import.cpp
+++ src/render/model_import.cpp
@@ -136,6 +136,9 @@ TextureHandle importSkybox(std::string filename, Render&) {
 
 	glCreateTextures(GL_TEXTURE_2D, 1, &texture);
 	glTextureStorage2D(texture, 1, GL_RGB16F, width, height);
+	glTextureParameteri(texture, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTextureParameteri(texture, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTextureParameteri(texture, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTextureSubImage2D(texture, 0, 0, 0, width, height, GL_RGB, GL_FLOAT, data);
 
 	stbi_image_free(data);
