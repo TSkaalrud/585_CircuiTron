132a1b34054b2f2c0edac57f75250bf05b74c0e5
Daniel Doran <ddoran3@gmail.com>
2021-03-19 21:31:31 -0600

    fix warning

--- src/render/model_import.hpp
+++ src/render/model_import.hpp
@@ -3,4 +3,8 @@
 #include "group.hpp"
 #include "import.hpp"
 
-#warning "model_import.hpp is renamed to import.hpp"
+#ifdef __GNUC__
+#warning("model_import.hpp is renamed to import.hpp")
+#else
+#pragma NOTE("model_import.hpp is renamed to import.hpp")
+#endif
